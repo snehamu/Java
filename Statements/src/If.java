0414927120d40b8759deaaab19ce9d968a670b27
snehamu <139531239+snehamu@users.noreply.github.com>
2023-07-21 13:37:24 +0530

    Add files via upload

---
+++ Statements/src/If.java
@@ -0,0 +1,10 @@
+public class If {
+    static int a=5;
+
+    public static void main(String[] args) {
+        if(a<10)
+        {
+            System.out.println("Given number is less than 10");
+        }
+    }
+}
---
+++ Statements/src/IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int num=5;
+        if(num%2==0) {
+            System.out.println("Given number is Even");
+        }
+        else{
+            System.out.println("Given number is Odd");
+        }
+    }
+}
